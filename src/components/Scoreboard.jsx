04661c4a16ed5261ca940d273f0678f78b357703
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-12 11:26:13 -0300

    Created components folder and Scoreboard component

---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import normalLogo from '../images/logo.svg';
+import bonusLogo from '../images/logo-bonus.svg';
+
+function Scoreboard ({ isNormal }) {
+
+  return (
+    <div className='scoreboard'>
+      <img src={isNormal ? normalLogo : bonusLogo} alt='logo' />
+      <div className='scoreboard-counter'>
+        <h2>Score</h2>
+        <p>0</p>
+      </div>
+    </div>
+  )
+  
+}
+
+export default Scoreboard;
