437d7c201c7a39f580c4ab71fb22c2bde2f9c2f6
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-17 16:17:21 -0300

    Show logo based on the gamemode functionality

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import normalLogo from '../images/logo.svg';
 import bonusLogo from '../images/logo-bonus.svg';
 
-function Scoreboard ({ isNormal }) {
+function Scoreboard () {
+  
+  let isNormal = true;
 
   return (
     <div className='scoreboard'>
