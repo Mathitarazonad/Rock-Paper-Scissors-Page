e9a8778e2ee3e6de9cfa20c195a0c6537637782c
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-18 16:16:00 -0300

    Changed the state to gamemodeSelected

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Menu from './components/Menu';
 import Game from './components/Game';
 
 function App() {
-  const gamemode = useSelector((store) => store.gamemodes.gamemode);
   const gamemodeSelected = useSelector(store => store.gamemodes.gamemodeSelected)
 
   return (
     <div className="App">
       {gamemodeSelected ? <Game /> : <Menu />}
       <footer> Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Mathias Tarazona</a>.</footer>
+    Coded by <a href="https://www.frontendmentor.io/profile/Mathitarazonad" target='_blank'>Mathias Tarazona</a>.</footer>
     </div>
   );
 }
