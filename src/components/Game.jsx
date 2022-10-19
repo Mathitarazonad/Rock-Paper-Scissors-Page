d2a4aad5bde5a8857a49ce10369c0aaf435c8dcf
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-19 16:03:49 -0300

    Added hands components based on the gamemode and fixed gamemode state name

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,21 +3,46 @@ import RulesChart from './RulesChart';
 import Scoreboard from './Scoreboard';
 import { useSelector, useDispatch } from 'react-redux';
 import { displayRules } from '../store/rulesSlice';
+import Hand from './Hand';
 
 function Game() {
   const dispatch = useDispatch();
   const showRules = useSelector((store) => store.rules.showRules);
-  const gamemode = useSelector((store) => store.gamemodes.gamemode);
+  const isNormalGame = useSelector((store) => store.gamemodes.normalGamemode);
+  const normalBtns = ['paper', 'scissors', 'rock'];
+  const bonusBtns = ['scissors', 'paper', 'rock', 'lizard', 'spock'];
 
   return (
     <main>
-      <div className="main-container">
+      <div className='main-container'>
         <Scoreboard />
+
+        <div
+          className={isNormalGame ? 'hands-container' : 'hands-container bonus'}
+        >
+          {isNormalGame ? (
+            <img
+              src={require('../images/bg-triangle.svg').default}
+              className='hands-bg'
+              alt='hands-bg'
+            />
+          ) : (
+            <img
+              src={require('../images/bg-pentagon.svg').default}
+              className='hands-bg'
+              alt='hands-bg'
+            />
+          )}
+          {isNormalGame
+            ? normalBtns.map((btn) => <Hand handType={btn} key={btn} />)
+            : bonusBtns.map((btn) => <Hand handType={btn} key={btn} />)}
+        </div>
+
         {showRules ? (
           <RulesChart />
         ) : (
           <button
-            className="show-rules-btn"
+            className='show-rules-btn'
             onClick={() => dispatch(displayRules())}
           >
             RULES
