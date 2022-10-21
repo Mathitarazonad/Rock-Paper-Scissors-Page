646ea6aaceb38ba57435edf48afbf93e350c1de1
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-21 17:58:20 -0300

    Render the available options or show the middle game stage

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,7 @@ import Scoreboard from './Scoreboard';
 import { useSelector, useDispatch } from 'react-redux';
 import { displayRules } from '../store/rulesSlice';
 import Hand from './Hand';
+import InGame from './InGame';
 
 function Game() {
   const dispatch = useDispatch();
@@ -11,38 +12,45 @@ function Game() {
   const isNormalGame = useSelector((store) => store.gamemodes.normalGamemode);
   const normalBtns = ['paper', 'scissors', 'rock'];
   const bonusBtns = ['scissors', 'paper', 'rock', 'lizard', 'spock'];
+  const userChoice = useSelector((store) => store.choices.userChoice);
 
   return (
     <main>
-      <div className='main-container'>
+      <div className="main-container">
         <Scoreboard />
 
-        <div
-          className={isNormalGame ? 'hands-container' : 'hands-container bonus'}
-        >
-          {isNormalGame ? (
-            <img
-              src={require('../images/bg-triangle.svg').default}
-              className='hands-bg'
-              alt='hands-bg'
-            />
-          ) : (
-            <img
-              src={require('../images/bg-pentagon.svg').default}
-              className='hands-bg'
-              alt='hands-bg'
-            />
-          )}
-          {isNormalGame
-            ? normalBtns.map((btn) => <Hand handType={btn} key={btn} />)
-            : bonusBtns.map((btn) => <Hand handType={btn} key={btn} />)}
-        </div>
+        {userChoice === '' ? (
+          <div
+            className={
+              isNormalGame ? 'hands-container' : 'hands-container bonus'
+            }
+          >
+            {isNormalGame ? (
+              <img
+                src={require('../images/bg-triangle.svg').default}
+                className="hands-bg"
+                alt="hands-bg"
+              />
+            ) : (
+              <img
+                src={require('../images/bg-pentagon.svg').default}
+                className="hands-bg"
+                alt="hands-bg"
+              />
+            )}
+            {isNormalGame
+              ? normalBtns.map((btn) => <Hand handType={btn} key={btn} clickable={true}/>)
+              : bonusBtns.map((btn) => <Hand handType={btn} key={btn} clickable={true}/>)}
+          </div>
+        ) : (
+          <InGame />
+        )}
 
         {showRules ? (
           <RulesChart />
         ) : (
           <button
-            className='show-rules-btn'
+            className="show-rules-btn"
             onClick={() => dispatch(displayRules())}
           >
             RULES
