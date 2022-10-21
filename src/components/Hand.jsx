296bb5b28427b8cd50a80c574ee3bd43b182602e
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-21 18:01:11 -0300

    Added clickable prop to Hand to avoid issues in middle game and refactored the click event to set both choice states at same time with a single function

--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import { setUserChoice } from '../store/choicesSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setComputerChoice } from '../store/choicesSlice';
 
-function Hand({ handType }) {
+function Hand({ handType, clickable }) {
   const dispatch = useDispatch();
+  const gamemode = useSelector(store => store.gamemodes.normalGamemode);
+  
+  function choiceHandler () {
+    //Computer random choice code
+    const buttons = ['rock', 'paper', 'scissors'];
 
-  return (
-    <div
-      className="single-hand"
-      id={handType}
-      onClick={() => dispatch(setUserChoice(handType))}
-    >
-      <img src={require(`../images/icon-${handType}.svg`)} alt={handType} />
-    </div>
-  );
+    if(!gamemode) {
+      buttons.push('lizard', 'spock')
+    }
+
+    const choice = buttons[Math.floor(Math.random()*buttons.length)];
+    dispatch(setComputerChoice(choice));
+
+    //Also set the choice state of the user
+    dispatch(setUserChoice(handType));
+  }
+
+  if (clickable) {
+    return (
+      <div className="single-hand" id={handType} onClick={() => choiceHandler ()}>
+        <img src={require(`../images/icon-${handType}.svg`)} alt={handType} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="single-hand" id={handType}>
+        <img src={require(`../images/icon-${handType}.svg`)} alt={handType} />
+      </div>
+    );
+  }
+  
 }
 
 export default Hand;
