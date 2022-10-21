d33674b563af7dd802a52bc106933fb589b57435
Mathias Tarazona <vladitarazona@gmail.com>
2022-10-21 16:13:34 -0300

    Added the onClick event to set the userChoice state

--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { setUserChoice } from '../store/choicesSlice';
+import { useDispatch } from 'react-redux';
 
 function Hand({ handType }) {
+  const dispatch = useDispatch();
+
   return (
-    <div className="single-hand" id={handType}>
+    <div
+      className="single-hand"
+      id={handType}
+      onClick={() => dispatch(setUserChoice(handType))}
+    >
       <img src={require(`../images/icon-${handType}.svg`)} alt={handType} />
     </div>
   );
